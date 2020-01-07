232261f31acfda2d9d9e145adcdf04f4f38f2873
Vernon-van-der-Merwe <42587105+Vernon-van-der-Merwe@users.noreply.github.com>
2020-01-07 08:43:41 +0200

    Macbook commit - added the rendered blogs and popular

---
+++ data/blogs.js
@@ -0,0 +1,56 @@
+let blogs = [{
+    'title' : 'Whats going on with humanity?',
+    'summary' : 'My take on why humanity is gong downhill',
+    'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum accusantium temporibus saepe dolore incidunt laboriosam repudiandae excepturi ipsa repellendus ipsam vitae odit sapiente in, ducimus hic necessitatibus distinctio quos quod itaque perspiciatis ipsum? Commodi, error blanditiis maiores, cumque doloremque officiis quas voluptate voluptas iste harum labore ipsum odit nemo libero nisi, id perspiciatis adipisci. Accusamus ipsa molestias eligendi eum reprehenderit veniam corporis cum voluptas illum quos? Necessitatibus iure voluptatibus vitae reiciendis aperiam at consectetur deserunt facere, ad ipsa, repudiandae tempore? Minus provident assumenda voluptates officia vero! Sapiente autem magnam natus impedit tempora repellat recusandae explicabo, ut, voluptatem alias maiores aliquid.',
+    'category' : 'Philosophy',
+    'created':'2019-02-12',
+    'modified': {},
+    'views' : ''
+
+},{
+    'title' : 'Why cant we ever come back?',
+    'summary' : 'My take on why humanity is gong downhill',
+    'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum accusantium temporibus saepe dolore incidunt laboriosam repudiandae excepturi ipsa repellendus ipsam vitae odit sapiente in, ducimus hic necessitatibus distinctio quos quod itaque perspiciatis ipsum? Commodi, error blanditiis maiores, cumque doloremque officiis quas voluptate voluptas iste harum labore ipsum odit nemo libero nisi, id perspiciatis adipisci. Accusamus ipsa molestias eligendi eum reprehenderit veniam corporis cum voluptas illum quos? Necessitatibus iure voluptatibus vitae reiciendis aperiam at consectetur deserunt facere, ad ipsa, repudiandae tempore? Minus provident assumenda voluptates officia vero! Sapiente autem magnam natus impedit tempora repellat recusandae explicabo, ut, voluptatem alias maiores aliquid.',
+    'category' : 'Philosophy',
+    'date':'2019-02-12',
+    'modified': {}
+
+},{
+    'title' : 'Mars is our only hope?',
+    'summary' : 'My take on why humanity is gong downhill',
+    'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum accusantium temporibus saepe dolore incidunt laboriosam repudiandae excepturi ipsa repellendus ipsam vitae odit sapiente in, ducimus hic necessitatibus distinctio quos quod itaque perspiciatis ipsum? Commodi, error blanditiis maiores, cumque doloremque officiis quas voluptate voluptas iste harum labore ipsum odit nemo libero nisi, id perspiciatis adipisci. Accusamus ipsa molestias eligendi eum reprehenderit veniam corporis cum voluptas illum quos? Necessitatibus iure voluptatibus vitae reiciendis aperiam at consectetur deserunt facere, ad ipsa, repudiandae tempore? Minus provident assumenda voluptates officia vero! Sapiente autem magnam natus impedit tempora repellat recusandae explicabo, ut, voluptatem alias maiores aliquid.',
+    'category' : 'Philosophy',
+    'date':'2019-02-12',
+    'modified': {},
+    'views' : ''
+
+},{
+    'title' : 'Elon musk will save humanity?',
+    'summary' : 'My take on why humanity is gong downhill',
+    'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum accusantium temporibus saepe dolore incidunt laboriosam repudiandae excepturi ipsa repellendus ipsam vitae odit sapiente in, ducimus hic necessitatibus distinctio quos quod itaque perspiciatis ipsum? Commodi, error blanditiis maiores, cumque doloremque officiis quas voluptate voluptas iste harum labore ipsum odit nemo libero nisi, id perspiciatis adipisci. Accusamus ipsa molestias eligendi eum reprehenderit veniam corporis cum voluptas illum quos? Necessitatibus iure voluptatibus vitae reiciendis aperiam at consectetur deserunt facere, ad ipsa, repudiandae tempore? Minus provident assumenda voluptates officia vero! Sapiente autem magnam natus impedit tempora repellat recusandae explicabo, ut, voluptatem alias maiores aliquid.',
+    'category' : 'Philosophy',
+    'date':'2019-02-12',
+    'modified': {},
+    'views' : ''
+
+},{
+    'title' : 'Is anyone else really lis for some KFC?',
+    'summary' : 'My take on why humanity is gong downhill',
+    'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum accusantium temporibus saepe dolore incidunt laboriosam repudiandae excepturi ipsa repellendus ipsam vitae odit sapiente in, ducimus hic necessitatibus distinctio quos quod itaque perspiciatis ipsum? Commodi, error blanditiis maiores, cumque doloremque officiis quas voluptate voluptas iste harum labore ipsum odit nemo libero nisi, id perspiciatis adipisci. Accusamus ipsa molestias eligendi eum reprehenderit veniam corporis cum voluptas illum quos? Necessitatibus iure voluptatibus vitae reiciendis aperiam at consectetur deserunt facere, ad ipsa, repudiandae tempore? Minus provident assumenda voluptates officia vero! Sapiente autem magnam natus impedit tempora repellat recusandae explicabo, ut, voluptatem alias maiores aliquid.',
+    'category' : 'Philosophy',
+    'date':'2019-02-12',
+    'modified': {},
+    'views' : ''
+
+},{
+    'title' : 'Writing blog titles become demanding after a while?',
+    'summary' : 'My take on why humanity is gong downhill',
+    'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum accusantium temporibus saepe dolore incidunt laboriosam repudiandae excepturi ipsa repellendus ipsam vitae odit sapiente in, ducimus hic necessitatibus distinctio quos quod itaque perspiciatis ipsum? Commodi, error blanditiis maiores, cumque doloremque officiis quas voluptate voluptas iste harum labore ipsum odit nemo libero nisi, id perspiciatis adipisci. Accusamus ipsa molestias eligendi eum reprehenderit veniam corporis cum voluptas illum quos? Necessitatibus iure voluptatibus vitae reiciendis aperiam at consectetur deserunt facere, ad ipsa, repudiandae tempore? Minus provident assumenda voluptates officia vero! Sapiente autem magnam natus impedit tempora repellat recusandae explicabo, ut, voluptatem alias maiores aliquid.',
+    'category' : 'Philosophy',
+    'date':'2019-02-12',
+    'modified': {},
+    'views' : ''
+
+}]
+
+export blogs;
--- index.js
+++ index.js
@@ -1,21 +1,108 @@
 //jshint using ES6
+let blogs = [{
+    'title': 'Whats going on with humanity?',
+    'summary': 'My take on why humanity is gong downhill',
+    'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum accusantium temporibus saepe dolore incidunt laboriosam repudiandae excepturi ipsa repellendus ipsam vitae odit sapiente in, ducimus hic necessitatibus distinctio quos quod itaque perspiciatis ipsum? Commodi, error blanditiis maiores, cumque doloremque officiis quas voluptate voluptas iste harum labore ipsum odit nemo libero nisi, id perspiciatis adipisci. Accusamus ipsa molestias eligendi eum reprehenderit veniam corporis cum voluptas illum quos? Necessitatibus iure voluptatibus vitae reiciendis aperiam at consectetur deserunt facere, ad ipsa, repudiandae tempore? Minus provident assumenda voluptates officia vero! Sapiente autem magnam natus impedit tempora repellat recusandae explicabo, ut, voluptatem alias maiores aliquid.',
+    'category': 'Philosophy',
+    'created': '2019-02-12',
+    'popular': false,
+    'modified': {},
+    'views': ''
 
+}, {
+    'title': 'Why cant we ever come back?',
+    'summary': 'My take on why humanity is gong downhill',
+    'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum accusantium temporibus saepe dolore incidunt laboriosam repudiandae excepturi ipsa repellendus ipsam vitae odit sapiente in, ducimus hic necessitatibus distinctio quos quod itaque perspiciatis ipsum? Commodi, error blanditiis maiores, cumque doloremque officiis quas voluptate voluptas iste harum labore ipsum odit nemo libero nisi, id perspiciatis adipisci. Accusamus ipsa molestias eligendi eum reprehenderit veniam corporis cum voluptas illum quos? Necessitatibus iure voluptatibus vitae reiciendis aperiam at consectetur deserunt facere, ad ipsa, repudiandae tempore? Minus provident assumenda voluptates officia vero! Sapiente autem magnam natus impedit tempora repellat recusandae explicabo, ut, voluptatem alias maiores aliquid.',
+    'category': 'Philosophy',
+    'popular': true,
+    'created': '2019-02-12',
+    'modified': {}
+
+}, {
+    'title': 'Mars is our only hope?',
+    'summary': 'My take on why humanity is gong downhill',
+    'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum accusantium temporibus saepe dolore incidunt laboriosam repudiandae excepturi ipsa repellendus ipsam vitae odit sapiente in, ducimus hic necessitatibus distinctio quos quod itaque perspiciatis ipsum? Commodi, error blanditiis maiores, cumque doloremque officiis quas voluptate voluptas iste harum labore ipsum odit nemo libero nisi, id perspiciatis adipisci. Accusamus ipsa molestias eligendi eum reprehenderit veniam corporis cum voluptas illum quos? Necessitatibus iure voluptatibus vitae reiciendis aperiam at consectetur deserunt facere, ad ipsa, repudiandae tempore? Minus provident assumenda voluptates officia vero! Sapiente autem magnam natus impedit tempora repellat recusandae explicabo, ut, voluptatem alias maiores aliquid.',
+    'category': 'Philosophy',
+    'created': '2019-02-12',
+    'popular': false,
+    'modified': {},
+    'views': ''
+
+}, {
+    'title': 'Elon musk will save humanity?',
+    'summary': 'My take on why humanity is gong downhill',
+    'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum accusantium temporibus saepe dolore incidunt laboriosam repudiandae excepturi ipsa repellendus ipsam vitae odit sapiente in, ducimus hic necessitatibus distinctio quos quod itaque perspiciatis ipsum? Commodi, error blanditiis maiores, cumque doloremque officiis quas voluptate voluptas iste harum labore ipsum odit nemo libero nisi, id perspiciatis adipisci. Accusamus ipsa molestias eligendi eum reprehenderit veniam corporis cum voluptas illum quos? Necessitatibus iure voluptatibus vitae reiciendis aperiam at consectetur deserunt facere, ad ipsa, repudiandae tempore? Minus provident assumenda voluptates officia vero! Sapiente autem magnam natus impedit tempora repellat recusandae explicabo, ut, voluptatem alias maiores aliquid.',
+    'category': 'Philosophy',
+    'created': '2019-02-12', 
+    'popular': true,
+    'modified': {},
+    'views': ''
+
+}, {
+    'title': 'Is anyone else really lis for some KFC?',
+    'summary': 'My take on why humanity is gong downhill',
+    'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum accusantium temporibus saepe dolore incidunt laboriosam repudiandae excepturi ipsa repellendus ipsam vitae odit sapiente in, ducimus hic necessitatibus distinctio quos quod itaque perspiciatis ipsum? Commodi, error blanditiis maiores, cumque doloremque officiis quas voluptate voluptas iste harum labore ipsum odit nemo libero nisi, id perspiciatis adipisci. Accusamus ipsa molestias eligendi eum reprehenderit veniam corporis cum voluptas illum quos? Necessitatibus iure voluptatibus vitae reiciendis aperiam at consectetur deserunt facere, ad ipsa, repudiandae tempore? Minus provident assumenda voluptates officia vero! Sapiente autem magnam natus impedit tempora repellat recusandae explicabo, ut, voluptatem alias maiores aliquid.',
+    'category': 'Philosophy',
+    'created': '2019-02-12', 
+    'popular': false,
+    'modified': {},
+    'views': ''
+
+}, {
+    'title': 'Writing blog titles become demanding after a while?',
+    'summary': 'My take on why humanity is gong downhill',
+    'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum accusantium temporibus saepe dolore incidunt laboriosam repudiandae excepturi ipsa repellendus ipsam vitae odit sapiente in, ducimus hic necessitatibus distinctio quos quod itaque perspiciatis ipsum? Commodi, error blanditiis maiores, cumque doloremque officiis quas voluptate voluptas iste harum labore ipsum odit nemo libero nisi, id perspiciatis adipisci. Accusamus ipsa molestias eligendi eum reprehenderit veniam corporis cum voluptas illum quos? Necessitatibus iure voluptatibus vitae reiciendis aperiam at consectetur deserunt facere, ad ipsa, repudiandae tempore? Minus provident assumenda voluptates officia vero! Sapiente autem magnam natus impedit tempora repellat recusandae explicabo, ut, voluptatem alias maiores aliquid.',
+    'category': 'Philosophy',
+    'popular': true,
+    'created': '2019-02-12', 
+    'modified': {},
+    'views': ''
+
+}]
+
+
+
+
+const _ = require('lodash');
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-app.get( "/" , (req,res) => {
+app.get("/", (req, res) => {
     console.log("get recieved");
-    res.render('landing');
+    res.render('landing', { blogs: blogs });
 });
 
 app.get('/read-blog', (req, res) => {
     res.render('read-blog');
-   });
+});
 
-app.listen(3000,() => console.log("listening on port 3000"));
+app.get('/compose', (req, res) => {
+    res.render('compose');
+});
+
+app.post('/compose', (req, res) => {
+    let article = {
+        'title': req.body.title,
+        'summary': req.body.summary,
+        'body': req.body.article_body,
+        'category': 'Philosophy',
+        'created': '2019-02-12',
+        'modified': {},
+        'views': ''
+    };
+    //Post article here
+});
+
+
+
+app.get('/blogs/:blog_id',(req,res) => {
+    console.log(req.params.blog_id);
+});
+
+app.listen(3000, () => console.log("listening on port 3000"));
 
